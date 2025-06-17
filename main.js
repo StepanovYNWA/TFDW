099a5f8ea2dcdeacf068ca1c10e5f2317ec57108
stepanovynwa <stepanov73rus@gmail.com>
2025-06-17 19:40:10 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿let gameState = {
+  player1Score: 0,
+  player2Score: 0,
+  currentNumber: null,
+  isGameActive: false,
+  soundEnabled: true,
+  isMobile: /Mobi|Android/i.test(navigator.userAgent)
+};
+
+let numberEl = document.getElementById('numberDisplay');
+let timeoutIds = [];
+const colors = ['white', 'yellow', 'lightgreen', 'lightblue', 'pink'];
+let roundTimer;
